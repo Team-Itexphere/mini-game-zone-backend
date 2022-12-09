c104163696de82a7e5bc501d74915248f66282ad
I-Chandana <indu.itexphere@gmail.com>
2022-12-09 17:03:01 +0530

    Dec09-V1

---
+++ models/game/game.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const gameSchema = mongoose.Schema({
+  name: String,
+  description: String,
+  imgUrl: String,
+  categories: String,
+  id: String,
+  gameUrl: String,
+  uploadDate: {
+    type: Date,
+    default: new Date(),
+  },
+  isRewardGame: String,
+  rating: [
+    {
+      userId: String,
+      ratedValue: Number,
+    },
+  ],
+});
+
+const Game = mongoose.model("GameData", gameSchema);
+
+export default Game;
